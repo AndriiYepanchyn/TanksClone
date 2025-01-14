03ed49a696968933a03d8812dcd20a2d56df0910
AYepanchyn <AYepanchyn@AYEPANCHYN-NB>
2025-01-15 00:51:17 +0200

    Lesson02_The worst method to draw the games

--- src/display/Display.java
+++ src/display/Display.java
@@ -1,10 +1,10 @@
-package src.display;
+package display;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Toolkit;
-import java.util.Properties;
-
 import javax.swing.JFrame;
 
 
@@ -13,13 +13,26 @@ public abstract class Display {
 	private static boolean isCreated = false;
 	private static JFrame window;
 	private static Canvas content;
+	private static int CANVAS_WIDHT;
+	private static int CANVAS_HEIGHT;
 	
 	public static void create(int width, int height, String title) {
 		if(isCreated) return;
+		CANVAS_WIDHT = width;
+		CANVAS_HEIGHT = height;
 		
 		Dimension size = new Dimension(width, height);
-		content = new Canvas();
+		content = new Canvas() {
+			@Override
+			public void paint(Graphics g) {
+//				Overriding paint is the worst way to draw dynamically changing objects
+//				This causes the blinking of redrawn objects
+				super.paint(g);
+				render (g);
+			}
+		};
 		content.setPreferredSize(size);
+		content.setBackground(Color.BLACK);
 		
 		window = new JFrame(title);
 		window.getContentPane().add(content);
@@ -30,5 +43,15 @@ public abstract class Display {
 		window.setVisible(true);
 		window.pack();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void render() {
+		content.repaint();
+	}
+	
+	private static void render(Graphics g) {
+		int r = 50;
+		g.setColor(Color.YELLOW);
+		g.fillOval(CANVAS_WIDHT/2-2*r, CANVAS_HEIGHT/2-2*r, 2*r, 2*r);
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,28 @@
-package src.main;
+package main;
 
-import src.display.Display;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Timer;
+
+import display.Display;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		Display.create(800, 600, "Tanks 1990 Clone");
+		
+		Timer timer = new Timer(1000/60, new AbstractAction() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Display.render();
+			}
+			
+		});
+		
+		timer.setRepeats(true);
+		timer.start();
 	}
 
 }
